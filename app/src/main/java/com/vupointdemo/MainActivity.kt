1bd361a9a10a23b87c84ac6b846e36cc77fbb735
Gursewak Singh <gursewak.singh@vuliv.com>
2018-03-05 14:15:52 +0530

    first commit

---
+++ app/src/main/java/com/vupointdemo/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.vupointdemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.vusharesdk.vushare.VuShare
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
